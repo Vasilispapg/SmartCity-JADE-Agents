5011e74f80b8903b75c582cd648046d0073159ff
Vasilis Papagrigoriou <vasilispapg@outlook.com>
2024-05-11 21:22:41 +0300

    Upgrade the city map

--- smartCity/CityMap.java
+++ smartCity/CityMap.java
@@ -1,17 +1,59 @@
 package examples.smartCity;
 
+import java.awt.Point;
 import java.util.Random;
 
 public class CityMap {
 
   protected String[][] grid;
+  private boolean[][] trafficMap; // Traffic map to track vehicle positions
+
   protected int size;
-  protected int blockSize = 8; // Smaller blocks within the 64x64 grid
+  protected int blockSize = 16; // Smaller blocks within the 64x64 grid
 
   public CityMap(int size) {
     this.size = size;
     this.grid = new String[size][size];
+    this.trafficMap = new boolean[size][size]; // Initialize traffic map
     initializeMap();
+  }
+
+  public boolean[][] getTrafficMap() {
+    return trafficMap;
+  }
+
+  public boolean isTrafficBlocked(Point position) {
+    if (withinBounds(position)) {
+      return trafficMap[position.x][position.y];
+    }
+    return false;
+  }
+
+  public void clearVehiclePosition(Point position) {
+    if (withinBounds(position)) {
+      trafficMap[position.x][position.y] = false;
+    }
+  }
+
+  public void setVehiclePosition(Point position) {
+    if (withinBounds(position) && canPlaceVehicle(position)) {
+      trafficMap[position.x][position.y] = true;
+    }
+  }
+
+  public boolean canPlaceVehicle(Point position) {
+    return withinBounds(position) && !trafficMap[position.x][position.y];
+  }
+
+  private boolean withinBounds(Point position) {
+    System.out.println("Checking bounds for position: " + position.toString());
+
+    return (
+      position.x >= 0 &&
+      position.x < size &&
+      position.y >= 0 &&
+      position.y < size
+    );
   }
 
   private void initializeMap() {
@@ -27,29 +69,6 @@ public class CityMap {
     System.out.println("Blocks" + size * size);
 
     System.out.println("Map initialized");
-
-    fillEmptyWithBuildings();
-  }
-
-  private void fillEmptyWithBuildings() {
-    Random rand = new Random();
-    for (int k = 0; k < size; k++) {
-      for (int l = 0; l < size; l++) {
-        if (grid[k][l].equals("Empty")) {
-          double chance = rand.nextDouble(); // Random chance between 0 and 1
-
-          if (chance < 0.15) {
-            grid[k][l] = "Fire Station"; // 15% for fire station
-          } else if (chance < 0.15 + 0.20) {
-            grid[k][l] = "Police Station"; // 20% for police station
-          } else if (chance < 0.15 + 0.20 + 0.25) {
-            grid[k][l] = "Hospital"; // 25% for hospital
-          } else {
-            grid[k][l] = "House"; // The rest for houses, which is 40%
-          }
-        }
-      }
-    }
   }
 
   private void clearMap() {
@@ -61,33 +80,63 @@ public class CityMap {
   }
 
   private void setupBlock(int x, int y) {
-    // Define the inner part of the block
+    defineRoadsAndSidewalks(x, y);
+    placeBuildingsAndCrosswalks(x, y);
+  }
+
+  private void defineRoadsAndSidewalks(int x, int y) {
     for (int i = 0; i < blockSize; i++) {
       for (int j = 0; j < blockSize; j++) {
         if (i == 0 || i == blockSize - 1 || j == 0 || j == blockSize - 1) {
-          grid[x + i][y + j] = "Road";
-        } else if (i == blockSize / 2 || j == blockSize / 2) {
-          // continue;
-        } else if (i == blockSize / 2 - 1 || j == blockSize / 2 - 1) {
-          grid[x + i][y + j] = "Sidewalk";
-        } else if (i == blockSize / 2 + 1 || j == blockSize / 2 + 1) {
-          grid[x + i][y + j] = "Sidewalk";
+          grid[x + i][y + j] = "Road - Straight Only";
         } else {
           grid[x + i][y + j] = "Sidewalk";
         }
       }
     }
+
+    // Roads for all possible movements
+    for (int i = 1; i < blockSize - 1; i++) {
+      grid[x][y + i] = "Road - Go Right Only"; //Right
+      grid[x + blockSize - 1][y + i] = "Road - Go Left Only"; // Left
+      grid[x + i][y] = "Road - Go Up Only";
+      grid[x + i][y + blockSize - 1] = "Road - Go Down Only";
+    }
   }
 
-  private void placeBuildings(String entity, int count) {
+  private void placeBuildingsAndCrosswalks(int x, int y) {
     Random rand = new Random();
-    for (int i = 0; i < count; i++) {
-      int x, y;
-      do {
-        x = rand.nextInt(size - 2) + 1; // Avoid the outermost road
-        y = rand.nextInt(size - 2) + 1; // Avoid the outermost road
-      } while (!grid[x][y].equals("Empty") || grid[x][y].equals("Sidewalk"));
-      grid[x][y] = entity;
+    // Assign buildings in the center of the block
+    for (int i = 1; i < blockSize - 1; i++) {
+      for (int j = 1; j < blockSize - 1; j++) {
+        if (i % (blockSize / 4) == 0 && j % (blockSize / 4) == 0) {
+          String building = selectBuilding(rand);
+          grid[x + i][y + j] = building;
+        }
+      }
+    }
+
+    // Add crosswalks at intersections
+    for (int i = 0; i < blockSize; i++) {
+      if (i == blockSize / 2 - 1 || i == blockSize / 2) {
+        grid[x + i][y] = "Crosswalk";
+        grid[x + i][y + blockSize - 1] = "Crosswalk";
+        grid[x][y + i] = "Crosswalk";
+        grid[x + blockSize - 1][y + i] = "Crosswalk";
+      }
+    }
+  }
+
+  private String selectBuilding(Random rand) {
+    double choice = rand.nextDouble();
+    if (choice < 0.25) {
+      return "Hospital";
+    } else if (choice < 0.50) {
+      return "Police Station";
+    } else if (choice < 0.75) {
+      return "Fire Station";
+    } else {
+      return "House";
     }
   }
 
