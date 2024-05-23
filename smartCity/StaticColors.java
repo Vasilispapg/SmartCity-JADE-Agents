d155a936c8ad2c90da03e2ea889c1b4e1c75cda2
Vasilis Papagrigoriou <vasilispapg@outlook.com>
2024-05-23 23:53:00 +0300

    Added colors

--- smartCity/StaticColors.java
+++ smartCity/StaticColors.java
@@ -16,7 +16,9 @@ public class StaticColors {
     colorMap.put("Citizen", Color.decode("#793690"));
     colorMap.put("NurseAgent", Color.WHITE);
     colorMap.put("Hospital", Color.WHITE);
+    colorMap.put("PoliceAgent", Color.BLUE);
     colorMap.put("Police Station", Color.BLUE);
+    colorMap.put("FireMan", Color.RED);
     colorMap.put("Fire Station", Color.RED);
     colorMap.put("Crosswalk", Color.YELLOW);
     colorMap.put("default", Color.BLACK);
